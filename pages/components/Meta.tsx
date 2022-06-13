09abbc969d23ac0a30c91df817ea8e60a202420f
Emmanuel Agyemang <emmanuelagyemang3@gmail.com>
2022-06-13 17:53:59 +0100

    sdfdsfdfsd

--- pages/components/Meta.tsx
+++ pages/components/Meta.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import Head from "next/head"
-import { Title } from '@/types/Types'
+import { Title } from '../types/Types'
 
 const Meta = ( { title }: Title ) => {
   return (
